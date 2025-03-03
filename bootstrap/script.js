70d6c9be53ec4b711d284ee5e316814dc8a5b75d
Pratham Sharma <148873155+prathammsharma@users.noreply.github.com>
2025-03-03 13:54:09 +0530

    Add files via upload

---
+++ bootstrap/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
